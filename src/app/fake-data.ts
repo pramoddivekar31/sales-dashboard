a294fd6ea459ffce8637544161853d72443f51ce
PramodGorakh Divekar <pramodgorakh.d@ad.infosys.com>
2020-10-26 20:04:40 +0530

    Feat : Initial setup

---
+++ src/app/fake-data.ts
@@ -0,0 +1,199 @@
+export const cardData = {
+  top: [
+    {
+      name: 'John Doe',
+      newMmr: 5230,
+      newLogos: 102,
+      demoCalls: 30
+    },
+    {
+      name: 'Jane Smith',
+      newMmr: 4586,
+      newLogos: 95,
+      demoCalls: 23
+    },
+    {
+      name: 'Ethan Hunt',
+      newMmr: 3500,
+      newLogos: 82,
+      demoCalls: 10
+    }
+  ],
+  bottom: [
+    {
+      name: 'Test Doe',
+      newMmr: 5230,
+      newLogos: 102,
+      demoCalls: 30
+    },
+    {
+      name: 'Test2 Smith',
+      newMmr: 4586,
+      newLogos: 95,
+      demoCalls: 23
+    },
+    {
+      name: 'Test3 Hunt',
+      newMmr: 3500,
+      newLogos: 82,
+      demoCalls: 10
+    }
+  ]
+}
+
+export const sellReps = [
+  {
+    id: 0,
+    leadIn: 25,
+    contactMade: 18,
+    needsDefined: 12,
+    proposalMade: 8,
+    negotiationsStrated: 5,
+    won: 3,
+    call: '10/15',
+    win: '1/3',
+    revenue: '80K/100K'
+  },
+  {
+    id: 1,
+    leadIn: 210,
+    contactMade: 18,
+    needsDefined: 12,
+    proposalMade: 8,
+    negotiationsStrated: 5,
+    won: 3,
+    call: '10/25',
+    win: '1/4',
+    revenue: '90K/100K'
+  }
+]
+
+export const tableData = [
+  {
+    id: 0,
+    salesRep: 'John Doe',
+    date: 'July 20th, 2018',
+    client: 'Oberio Group of Hotels',
+    contactPerson: 'Mahmud Ritelli',
+    Title: 'C level',
+    verticle: 'Marketing',
+    meetingLocation: 'Goto Meeting',
+    perposeOfInteraction: 'Understand their current ecosystem',
+    remarks: 'Send first cut of proposed solution',
+    engagementStatus: 'Needs defined'
+  },
+  {
+    id: 1,
+    salesRep: 'Ethan Hunt',
+    date: 'July 20th, 2018',
+    client: 'Taj Group of Hotels',
+    contactPerson: 'Avie Peasegood',
+    Title: 'C level',
+    verticle: 'Sales',
+    meetingLocation: 'Video',
+    perposeOfInteraction: 'Demo the product',
+    remarks: 'She will get back with availibility',
+    engagementStatus: 'Contact Made'
+  }, {
+    id: 2,
+    salesRep: 'John Doe',
+    date: 'July 20th, 2018',
+    client: 'Accenture',
+    contactPerson: 'Dew Tilbey',
+    Title: 'C level',
+    verticle: 'Senior Management',
+    meetingLocation: 'Face to face',
+    perposeOfInteraction: 'Negotiations',
+    remarks: 'He needs to get final approval by the board. Will happen by today EOD',
+    engagementStatus: 'Negotiations started'
+  },
+  {
+    id: 3,
+    salesRep: 'Julia Cruise',
+    date: 'July 20th, 2018',
+    client: 'Google',
+    contactPerson: 'Mahmud Ritelli',
+    Title: 'C level',
+    verticle: 'Marketing',
+    meetingLocation: 'Goto Meeting',
+    perposeOfInteraction: 'Understand their current ecosystem',
+    remarks: 'Send first cut of proposed solution',
+    engagementStatus: 'Needs defined'
+  },
+  {
+    id: 4,
+    salesRep: 'Tom Roberts',
+    date: 'July 20th, 2018',
+    client: 'Microsoft',
+    contactPerson: 'Avie Peasegood',
+    Title: 'C level',
+    verticle: 'Sales',
+    meetingLocation: 'Video',
+    perposeOfInteraction: 'Demo the product',
+    remarks: 'She will get back with availibility',
+    engagementStatus: 'Contact Made'
+  }, {
+    id: 5,
+    salesRep: 'John Doe',
+    date: 'July 20th, 2018',
+    client: 'Accenture',
+    contactPerson: 'Dew Tilbey',
+    Title: 'C level',
+    verticle: 'Senior Management',
+    meetingLocation: 'Face to face',
+    perposeOfInteraction: 'Negotiations',
+    remarks: 'He needs to get final approval by the board. Will happen by today EOD',
+    engagementStatus: 'Negotiations started'
+  },
+  {
+    id: 6,
+    salesRep: 'Ethan Hunt',
+    date: 'July 20th, 2018',
+    client: 'Oberio Group of Hotels',
+    contactPerson: 'Mahmud Ritelli',
+    Title: 'C level',
+    verticle: 'Marketing',
+    meetingLocation: 'Goto Meeting',
+    perposeOfInteraction: 'Understand their current ecosystem',
+    remarks: 'Send first cut of proposed solution',
+    engagementStatus: 'Needs defined'
+  },
+  {
+    id: 7,
+    salesRep: 'Jane Doe',
+    date: 'July 20th, 2018',
+    client: 'Taj Group of Hotels',
+    contactPerson: 'Avie Peasegood',
+    Title: 'C level',
+    verticle: 'Sales',
+    meetingLocation: 'Video',
+    perposeOfInteraction: 'Demo the product',
+    remarks: 'She will get back with availibility',
+    engagementStatus: 'Contact Made'
+  }, {
+    id: 8,
+    salesRep: 'Julia Cruise',
+    date: 'July 20th, 2018',
+    client: 'Accenture',
+    contactPerson: 'Dew Tilbey',
+    Title: 'C level',
+    verticle: 'Senior Management',
+    meetingLocation: 'Face to face',
+    perposeOfInteraction: 'Negotiations',
+    remarks: 'He needs to get final approval by the board. Will happen by today EOD',
+    engagementStatus: 'Negotiations started'
+  },
+  {
+    id: 9,
+    salesRep: 'Tom Roberts',
+    date: 'July 20th, 2018',
+    client: 'Oberio Group of Hotels',
+    contactPerson: 'Mahmud Ritelli',
+    Title: 'C level',
+    verticle: 'Marketing',
+    meetingLocation: 'Goto Meeting',
+    perposeOfInteraction: 'Understand their current ecosystem',
+    remarks: 'Send first cut of proposed solution',
+    engagementStatus: 'Needs defined'
+  }
+]
